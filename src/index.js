a1c684fab37a3f0c4190fa0a44f08bf3466fcadf
Zz <quitjie@gmail.com>
2020-07-21 17:44:25 +0800

    fix log

--- src/index.js
+++ src/index.js
@@ -15,7 +15,11 @@ module.exports = (baseConfig = {}) => {
   extendedConfig.internal = extendedConfig.internal || {}
   extendedConfig.internal.logger = createSenecaLogger(customLogger, logLevel)
 
-  const config = Object.assign({}, baseConfig, extendedConfig)
+  const config = Object.assign({
+    log: {
+      level: logLevel
+    }
+  }, baseConfig, extendedConfig)
   const seneca = require('seneca')(config)
 
   // append additional methods (promisified actions, error emitters, clean loggers etc)
